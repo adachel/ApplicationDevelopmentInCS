848f678ee1c32a88b94a6fa85fba33a94ca189d5
adachel <adachel74@gmail.com>
2024-03-03 16:19:23 +0500

    add Dir Lec4

---
+++ Lections/Lection4/MainLec4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentInCS.Lections.Lection4
+{
+    internal class MainLec4
+    {
+        public void Run()
+        { 
+        
+        
+        
+        }
+    }
+}
